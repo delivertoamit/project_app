bf0f6cf662a451ad27c8e5a53ba3f8385cc7bdb0
Amit Kumar <delivertoamit@gmail.com>
2022-03-24 15:27:48 +0530

    metrics

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src", 
+    version="0.0.0", 
+    description="Wine package demo", 
+    author="amit", 
+    package=find_packages(), 
+    license='MIT'
+)
+
